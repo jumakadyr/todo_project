f0159b4b73272ae16c94ed2c388caabaf516dc62
Juma <jums92547@gmail.com>
2025-03-09 03:13:51 -0500

    almost finished everything

--- config/settings.py
+++ config/settings.py
@@ -125,10 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 
